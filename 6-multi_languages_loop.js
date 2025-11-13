ac046ec7dd5386b0f028de7c22da5028a70b394e
AbasifrekeEffi <sirabas146@gmail.com>
2025-11-13 18:44:57 +0100

    file 6

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const myStr = ["C is fun", "Python is cool", "Javascript is amazing"];
-for(let i=0;i<myStr.length;i++){
-    console.log(myStr[i]);
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for(let i=0; i < lines.length; i++){
+    console.log(lines[i]);
 }
