f6b8319b430574dc8e27e8962b902cdea54986a1
AbasifrekeEffi <sirabas146@gmail.com>
2025-11-13 11:51:27 +0100

    ok

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,7 @@
-const C = "C is fun";
-const Python = "Python is cool";
-const Javascript = "Javascript is amazing";
-console.log(C);
-console.log(Python);
-console.log(Javascript);
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
